6680f4a8d4f9d8e2e409af24f6100b8a7a1aa4bc
Elzenaro <nam2626@naver.com>
2022-07-28 14:40:16 +0900

    mvc mybatis example

--- SpringMVCMybatis/src/main/java/com/example/MainController.java
+++ SpringMVCMybatis/src/main/java/com/example/MainController.java
@@ -2,7 +2,18 @@ package com.example;
 
 import org.springframework.stereotype.Controller;
 
+import com.example.service.MemberService;
+
 @Controller
 public class MainController {
+	private MemberService service;
 
+	public MainController(MemberService service) {
+		this.service = service;
+	}
+	
+	
 }
+
+
+
--- SpringMVCMybatis/src/main/java/com/example/service/MemberService.java
+++ SpringMVCMybatis/src/main/java/com/example/service/MemberService.java
@@ -2,7 +2,16 @@ package com.example.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.mapper.MemberMapper;
+
 @Service
 public class MemberService {
+	private MemberMapper mapper;
 
+	public MemberService(MemberMapper mapper) {
+		super();
+		this.mapper = mapper;
+	}
+	
+	
 }
