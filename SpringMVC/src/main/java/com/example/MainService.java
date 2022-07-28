e81d1f955532e19ef1821b2703853a255825177e
Elzenaro <nam2626@naver.com>
2022-07-28 11:50:09 +0900

    mvc example

---
+++ SpringMVC/src/main/java/com/example/MainService.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainService {
+
+	public void testService() {
+		System.out.println("testService method");
+	}
+}
