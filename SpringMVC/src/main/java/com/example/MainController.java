de32722cd6d142fdf5c92f72c2e062de12b6bbcb
Elzenaro <nam2626@naver.com>
2022-07-28 12:05:20 +0900

    mvc example

--- SpringMVC/src/main/java/com/example/MainController.java
+++ SpringMVC/src/main/java/com/example/MainController.java
@@ -3,16 +3,23 @@ package com.example;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+	private MainService service;
+	
+	public MainController(MainService service) {
+		this.service = service;
+	}
 
 	@RequestMapping("/")
 	public String test() {
 		System.out.println("test()");
+		service.testService();
 		return "test";
 	}
 	
