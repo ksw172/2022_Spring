163011be09ca40967262765cd72868c314162146
Elzenaro <nam2626@naver.com>
2022-07-29 15:53:02 +0900

    Spring Board Project

--- SpringBoardProject/src/main/java/com/koreait/MainController.java
+++ SpringBoardProject/src/main/java/com/koreait/MainController.java
@@ -1,10 +1,16 @@
 package com.koreait;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.koreait.dto.BoardDTO;
 import com.koreait.service.BoardService;
 import com.koreait.service.MemberService;
+import com.koreait.vo.PaggingVO;
 
 @Controller
 public class MainController {
@@ -17,7 +23,17 @@ public class MainController {
 	}
 	
 	@RequestMapping("/")
-	public String main() {
+	public String main(@RequestParam(name = "pageNo",defaultValue = "1") int pageNo,
+			Model model) {
+//		System.out.println(pageNo);
+		List<BoardDTO> list = boardService.selectBoardList(pageNo);
+		model.addAttribute("list", list);
+		
+		//페이징 처리
+		int count = boardService.selectBoardCount();
+		PaggingVO vo = new PaggingVO(count,pageNo,10,5);
+		model.addAttribute("pagging", vo);
+		
 		return "main";
 	}
 	
