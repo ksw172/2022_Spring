23b10db082ed03f78777b886dc48593d7cd88215
Elzenaro <nam2626@naver.com>
2022-07-29 14:15:42 +0900

    Spring Board Project

---
+++ SpringBoardProject/src/main/java/com/koreait/SpringBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.koreait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoardProjectApplication.class, args);
+	}
+
+}
